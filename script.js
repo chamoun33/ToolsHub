e659188fee2c713cf5b24abb2d02f2b82c3b1dbf
chamoun33 <168731876+chamoun33@users.noreply.github.com>
2024-09-24 07:58:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -801,7 +801,7 @@ function addToCart(item, item_img, image, item_description, item_name, name, SKU
 
 function scrollButton(sectionID){
     const target = document.getElementById(sectionID);
-    const offset = 70;
+    const offset = 100;
     window.scrollTo({
                 top: target.offsetTop - offset,
                 behavior: 'smooth'
