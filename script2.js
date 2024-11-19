4c5ef6f3ff6c0ce0a5f673d3cc88d800ec60ddc5
chamoun33 <168731876+chamoun33@users.noreply.github.com>
2024-11-19 21:43:10 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,17 +1,19 @@
-let score;
-let checkQuestions;
+let score = 0;
+let checkQuestions = 0;
 let condition;
+let API = '';
+let firstPartAPI = 'https://the-trivia-api.com/api/questions?categories=';
+let secondPartAPI = '';
+let numberOfQuestions;
+let category = 'General Knowledge', difficulty = 'Easy';
+let categoryAPI = 'general_knowledge', difficultyAPi = 'easy';
 
-function startQuiz(numOfQuestions, cond){
 
-    let numberOfQuestions = numOfQuestions;
-    checkQuestions = 0;
-    condition = cond;
-    score = 0;
 
-    console.log('startQuiz started');
+    
+    
 
-    fetchQuestion();
+
     // Fetch question from TriviaDB API
     function fetchQuestion() {
 
@@ -21,6 +23,10 @@ function startQuiz(numOfQuestions, cond){
                 .then(response => response.json())
                 .then(data => {
                     const questionData = data[0];
+                    document.getElementById('quizContainer').style.display = 'flex';
+                    document.getElementById('loading-page').style.display = 'none';
+                    document.getElementById('info-container').style.display = 'flex'; 
+                    document.getElementById('score-info-container').style.display = 'flex';
                     displayQuestion(questionData);
                 })
                 .catch(error => console.error('Error fetching question:', error));
@@ -34,15 +40,28 @@ function startQuiz(numOfQuestions, cond){
         
         if(condition === 'unlimited'){
 
-            document.getElementById('exit-unlimitedQuiz-container').style.display = 'flex';
+            // document.getElementById('mainBox').style.display = 'none';
+            // document.getElementById('loading-container').style.display= 'block';
 
-            fetch('https://the-trivia-api.com/api/questions?categories=geography&limit=1')
+            
+
+            fetch(API)
                 .then(response => response.json())
                 .then(data => {
                     const questionData = data[0];
                     displayQuestion(questionData);
+                    document.getElementById('exit-unlimitedQuiz-container').style.display = 'flex';
+                    document.getElementById('quizContainer').style.display = 'flex';
+                    document.getElementById('loading-page').style.display = 'none';
+                    document.getElementById('info-container').style.display = 'flex'; 
+                    document.getElementById('score-info-container').style.display = 'flex';
                 })
                 .catch(error => console.error('Error fetching question:', error));
+
+                // document.getElementById('mainBox').style.display = 'block';
+                // document.getElementById('loading-container').style.display= 'none';
+
+
                 checkQuestions ++;
                 console.log('Question questionned: ' + checkQuestions);
         }
@@ -52,9 +71,18 @@ function startQuiz(numOfQuestions, cond){
 
     // Display question and answers
     function displayQuestion(questionData) {
-        document.getElementById('quiz-number-question').style.display = 'none';
         const questionElement = document.getElementById('question');
         const answersContainer = document.getElementById('answers');
+
+
+        
+        document.getElementById('category-info').innerHTML = 'Category: ' + category;
+        document.getElementById('difficulty-info').innerHTML = 'Difficulty: ' + difficulty;
+        document.getElementById('question-counter-info').innerHTML = 'Question: ' + (checkQuestions) + (condition === 'limited' ? '/' +  numberOfQuestions : '');
+        document.getElementById('score-info').innerHTML = 'Score: ' + score + (condition === 'limited' ? '/' +  (numberOfQuestions * 10) : '');
+
+
+        console.log(questionData.question);
 
         questionElement.innerHTML = questionData.question;
         answersContainer.innerHTML = '';
@@ -83,7 +111,7 @@ function startQuiz(numOfQuestions, cond){
             setTimeout(() => {
                 document.getElementById('mainBox').classList.remove("highlighted-green");
             }, (2000));
-            score ++;
+            score += 10;
             console.log('Score: ' + score);
             // setTimeout(fetchQuestion, 800);
             fetchQuestion();
@@ -103,21 +131,10 @@ function startQuiz(numOfQuestions, cond){
 
     
 
-}
 
 
 
-function switchUI(){
-    console.log("UI switched!");
 
-    document.getElementById('quote').style.display = 'none';
-    document.getElementById('question01').style.display = 'none';
-    document.getElementById('question').style.display = 'block';
-    document.getElementById('quiz-number-question').style.display = 'block';
-    document.getElementById('quizForm').style.display = 'flex';
-    document.getElementById('unlimitedQuestionContainer').style.display = 'block';
-    document.getElementById('FirstUI-button').style.display = 'none';
-}
 
 
 
@@ -127,35 +144,217 @@ document.getElementById('quizForm').addEventListener('submit', function(event) {
     // Get the number of questions from the input field
     let numQuestions = document.getElementById('numQuestions').value;
     console.log('number of questions: ' + numQuestions);
-    startQuiz(numQuestions, 'limited');
+    numberOfQuestions = numQuestions;
+    document.getElementById('numberOfQuestions').style.display = 'none';   
+    document.getElementById('loading-page').style.display = 'flex';
+    condition = 'limited';
+    document.getElementById('numQuestions').value = null;
+    fetchQuestion();
 });
 
 
 function startUnlimitedQuestion(){
-    startQuiz(0, 'unlimited');
+    document.getElementById('numberOfQuestions').style.display = 'none';
+    document.getElementById('loading-page').style.display = 'flex';
+    condition = 'unlimited';
+    fetchQuestion();
 }
 
 
 function displayResult(){
-    document.getElementById('mainBox').style.border = '2px solid #6a53eb';
-    document.getElementById('finalScore').style.display = 'block';
+    document.getElementById('quizContainer').style.display = 'none';
+    document.getElementById('final-score-container').style.display = 'flex';
+    document.getElementById('info-container').style.display = 'none';
+    document.getElementById('score-info-container').style.display = 'none';
+
+    if(condition === 'limited'){
+        setTimeout(() => 
+            document.getElementById('mainBox').style.border = '2px solid #6a53eb'
+        , 2000);
+    }   
+    else{
+        document.getElementById('mainBox').style.border = '2px solid #6a53eb';
+    }
+
+
     let displayScore = document.getElementById('score');
-    displayScore.innerHTML = score + '/' + (condition == 'unlimited' ? checkQuestions - 1 : checkQuestions);
-    displayScore.style.display = 'block';
-    document.getElementById('question').style.display = 'none';
-    document.getElementById('answers').style.display = 'none';
-    document.getElementById('restart-quote').style.display = 'block';
-    document.getElementById('restartQuiz').style.display = 'block';
-    document.getElementById('fialUI-id').style.display = 'flex';
-    document.getElementById('exit-unlimitedQuiz-container').style.display = 'none';
+    displayScore.innerHTML = score + '/' + (condition == 'unlimited' ? ((checkQuestions - 1)*10) : (checkQuestions*10));
 }
 
+
+
+function switchUI_1(){
+    console.log("UI 1 switched!");
+
+    document.getElementById('firstUI').style.display = 'none';
+    document.getElementById('settings').style.display = 'flex';
+}
+
+
 function switchUI_2(){
-    document.getElementById('finalScore').style.display = 'none';
-    document.getElementById('score').style.display = 'none';
-    document.getElementById('fialUI-id').style.display = 'none';
-    document.getElementById('question').style.display = 'block';
-    document.getElementById('quiz-number-question').style.display = 'block';
-    document.getElementById('quizForm').style.display = 'flex';
-    document.getElementById('unlimitedQuestionContainer').style.display = 'block';
+    console.log("UI 2 switched!");
+
+    document.getElementById('numberOfQuestions').style.display = 'flex';
+    document.getElementById('settings').style.display = 'none';
+}
+
+
+function restartQuiz(){
+    score = 0;
+    checkQuestions = 0;
+    firstPartAPI = 'https://the-trivia-api.com/api/questions?categories=';
+    secondPartAPI = '';
+    
+    document.getElementById('final-score-container').style.display = 'none';
+    document.getElementById('loading-page').style.display = 'flex';
+    document.getElementById('mainBox').style.border = '2px solid #000';
+    document.getElementById('settingICON').style.visibility = 'visible';
+    fetchQuestion();
+}
+
+
+function editSettings(){
+    score = 0;
+    checkQuestions = 0;
+    firstPartAPI = 'https://the-trivia-api.com/api/questions?categories=';
+    secondPartAPI = '';
+
+
+    document.getElementById('quizContainer').style.display = 'none';
+    document.getElementById('settingICON').style.visibility = 'hidden';
+    document.getElementById('settings').style.display = 'flex';
+    document.getElementById('info-container').style.display = 'none';
+    document.getElementById('score-info-container').style.display = 'none';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function openDropdown(dropdown1,id) {
+    const dropdowns = document.querySelectorAll('.dropdown');
+    const currentDropdown = document.getElementById(id);
+    let otherDP="";
+    if(dropdown1.id=="Topic"){
+  otherDP=document.getElementById("difficulty-dropdown");
+    }
+    else{
+      otherDP=document.getElementById("Topic");
+    }
+  
+  
+    // Close all dropdowns except the one being opened
+    dropdowns.forEach(dropdown => {
+      if (dropdown !== currentDropdown) {
+        dropdown.classList.remove('open');
+    
+      }
+     
+  
+      
+    });
+  
+    if (currentDropdown.classList.contains('open')) {
+      currentDropdown.classList.remove('open');
+      dropdown1.style.backgroundColor="#333";
+      dropdown1.style.borderColor="#666";
+  
+    } else {
+      currentDropdown.classList.add('open');
+      dropdown1.style.backgroundColor="rgb(100, 109, 237)";
+      dropdown1.style.borderColor="white";
+    }
+  
+  
+  }
+
+  // Function to call when clicking outside
+function handleClickOutside() {
+    let d1=document.getElementById('difficulty-dropdown');
+    let d2=document.getElementById('Topic');
+    // if (!container.contains(event.target)&& document.getElementById("settings").style.display!="none") {
+    //   difficulty="easy";
+    //   category=9;
+    //   d1.style.backgroundColor="#333";
+    //   d1.style.borderColor="#666";
+    //   d1.innerHTML="<span style=\"padding-left: 5px;\" id=\"difficultyPlaceholder\">Select Difficulty</span><i class=\"fa-solid fa-chevron-down\" style=\"font-size: 11px;\"></i>"
+    //   d2.style.backgroundColor="#333";
+    //   d2.style.borderColor="#666";
+    //   d2.innerHTML="<span style=\"padding-left: 5px;\" id=\"topicPlaceholder\">Select Topic</span><i class=\"fa-solid fa-chevron-down\" style=\"font-size: 11px;\"></i>"
+    // }
+
+    for(let i=0;i<2;i++){
+        document.getElementsByClassName("dropdown")[i].classList.remove('open');
+      }
+  }
+  
+  // Add event listener to the document
+//   document.addEventListener('click', handleClickOutside);
+
+
+
+
+  
+
+
+
+
+
+
+
+function modifyAPIdiff(difficult){
+    difficultyAPi = difficult;
+    document.getElementById('difficultyPlaceholder').innerHTML = difficult.charAt(0).toUpperCase() + difficult.slice(1);
+    difficulty = difficult.charAt(0).toUpperCase() + difficult.slice(1);
+
+    const dropdowns = document.querySelectorAll('.dropdown');
+    const currentDropdown = document.getElementById('diff-dp');
+
+    dropdowns.forEach(dropdown => {
+        if (dropdown === currentDropdown) {
+          dropdown.classList.remove('open');
+      
+        }
+      });
+
+
+}
+
+function modifyAPI(Category, name){
+    categoryAPI = Category;
+    document.getElementById('topicPlaceholder').innerHTML = name;
+
+    category = name;
+
+    const dropdowns = document.querySelectorAll('.dropdown');
+    const currentDropdown = document.getElementById('top-dp');
+
+    dropdowns.forEach(dropdown => {
+        if (dropdown === currentDropdown) {
+          dropdown.classList.remove('open');
+      
+        }
+      });
+}
+
+function createAPI(){
+    secondPartAPI += '&difficulty=' + difficultyAPi + '&limit=1';
+    firstPartAPI += categoryAPI; 
+
+    API = firstPartAPI + secondPartAPI;
+
+    console.log(API);
+    switchUI_2();
 }
