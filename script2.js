882ec07ac1509cd287d43534cf8dc1807ad73aba
chamoun33 <168731876+chamoun33@users.noreply.github.com>
2024-11-22 17:12:48 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -12,8 +12,11 @@ let currentQuestion;
 let answer01, answer02, answer03, answer04;
 let CorrectAnswer;
 let questions = [];
-let printed;
-Boolean(printed);
+let printed, checkTimeStart;
+Boolean(printed, checkTimeStart);
+checkTimeStart = false;
+const timer = document.getElementById('checkbox');
+
 
     
     
@@ -21,6 +24,7 @@ Boolean(printed);
 
     // Fetch question from TriviaDB API
     function fetchQuestion() {
+
 
         if (condition === 'limited'){
             if (checkQuestions < numberOfQuestions){
@@ -73,6 +77,21 @@ Boolean(printed);
     function displayQuestion(questionData) {
         const questionElement = document.getElementById('question');
         const answersContainer = document.getElementById('answers');
+
+        if(!checkTimeStart){
+            if(timer.checked){
+                if(condition === 'limited'){
+                    timerDuration = numberOfQuestions * 5;
+                }
+                else{
+                    timerDuration = 300;
+                }
+                startTimer();
+                document.getElementById('timerContainer').style.display = 'flex';
+            }
+            timerDisplay.style.color = '#D3D3D3';
+            checkTimeStart = true;
+        }
 
 
         
@@ -156,6 +175,62 @@ Boolean(printed);
     }
 
 
+
+
+// Set the initial countdown duration in seconds
+let timerDuration;
+let countdown; // Variable to hold the timer interval
+
+// Select elements
+const timerDisplay = document.getElementById("timer");
+
+// Function to format time in MM:SS
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+
+    return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+}
+
+// Function to start the timer
+function startTimer() {
+    timerDisplay.textContent = formatTime(timerDuration); // Initialize display
+
+    countdown = setInterval(() => {
+        timerDuration--;
+
+        if (timerDuration >= 0) {
+            timerDisplay.textContent = formatTime(timerDuration); // Update the display
+        }
+        else {
+            clearInterval(countdown); // Stop the timer
+            
+
+            if(condition === 'limited'){
+                document.getElementById('outOfTime').style.display = 'block';
+                displayResult();
+            }
+            else{
+                displayResult();
+            }
+            
+        }
+
+        if(((timerDuration === 3) && (condition === 'limited')) || ((timerDuration === 10) && (condition === 'unlimited'))){
+            timerDisplay.style.color = '#dc3545';
+        }
+
+
+    }, 1000);
+}
+
+    
+
+
+
+
+
+
     function DisplaySummaryQuestions(){
 
         
@@ -167,105 +242,105 @@ Boolean(printed);
 
 
             printed = true;
-                questions.forEach(q => {
-                    const mainConatiner = document.getElementById('summaryMainBoxContainer');
+            questions.forEach(q => {
+                const mainConatiner = document.getElementById('summaryMainBoxContainer');
 
-                    const mainQuestionContainer = document.createElement('div');
-                    mainQuestionContainer.className = 'summary-question-result-container';
+                const mainQuestionContainer = document.createElement('div');
+                mainQuestionContainer.className = 'summary-question-result-container';
 
-                    const questionContainer = document.createElement('div');
-                    questionContainer.className = 'summary-question-container';
+                const questionContainer = document.createElement('div');
+                questionContainer.className = 'summary-question-container';
 
-                    const questionn = document.createElement('p');
-                    questionn.className = 'SummaryQuestion';
-                    questionn.textContent = q.questionNbr + ' - ' + q.question;
+                const questionn = document.createElement('p');
+                questionn.className = 'SummaryQuestion';
+                questionn.textContent = q.questionNbr + ' - ' + q.question;
 
-                    const answersContainer = document.createElement('div');
-                    answersContainer.className = 'summary-answers-container';
+                const answersContainer = document.createElement('div');
+                answersContainer.className = 'summary-answers-container';
 
-                    const answer_1 = document.createElement('p');
-                    answer_1.textContent = '● \u00A0' + q.answer1;
+                const answer_1 = document.createElement('p');
+                answer_1.textContent = '● \u00A0' + q.answer1;
 
-                    const answer_2 = document.createElement('p');
-                    answer_2.textContent = '● \u00A0' + q.answer2;
+                const answer_2 = document.createElement('p');
+                answer_2.textContent = '● \u00A0' + q.answer2;
 
-                    const answer_3 = document.createElement('p');
-                    answer_3.textContent = '● \u00A0' + q.answer3;
+                const answer_3 = document.createElement('p');
+                answer_3.textContent = '● \u00A0' + q.answer3;
 
-                    const answer_4 = document.createElement('p');
-                    answer_4.textContent = '● \u00A0' + q.answer4;
+                const answer_4 = document.createElement('p');
+                answer_4.textContent = '● \u00A0' + q.answer4;
 
-                    const score = document.createElement('p');
-                    
-                    if(q.choosedAnswer === q.correctAnswer){
-                        score.className = 'summaryPassedQuestionScore';
-                        score.textContent = '+ 10';
+                const score = document.createElement('p');
+                
+                if(q.choosedAnswer === q.correctAnswer){
+                    score.className = 'summaryPassedQuestionScore';
+                    score.textContent = '+ 10';
 
-                        mainQuestionContainer.classList.add('green');
+                    mainQuestionContainer.classList.add('green');
 
-                        if(q.choosedAnswer === q.answer1){
-                            answer_1.classList.add('answerHighlightedGreen');
-                        }
-                        else if(q.choosedAnswer === q.answer2){
-                            answer_2.classList.add('answerHighlightedGreen');
-                        }
-                        else if(q.choosedAnswer === q.answer3){
-                            answer_3.classList.add('answerHighlightedGreen');
-                        }
-                        else{
-                            answer_4.classList.add('answerHighlightedGreen');
-                        }
-
-                        correctAnswersCounter ++;
+                    if(q.choosedAnswer === q.answer1){
+                        answer_1.classList.add('answerHighlightedGreen');
+                    }
+                    else if(q.choosedAnswer === q.answer2){
+                        answer_2.classList.add('answerHighlightedGreen');
+                    }
+                    else if(q.choosedAnswer === q.answer3){
+                        answer_3.classList.add('answerHighlightedGreen');
                     }
                     else{
-                        score.className = 'summaryFailedQuestionScore';
-                        score.textContent = '+ 0';
-                        mainQuestionContainer.classList.add('red');
-
-                        if(q.choosedAnswer === q.answer1){
-                            answer_1.classList.add('answerHighlightedRed');
-                        }
-                        else if(q.choosedAnswer === q.answer2){
-                            answer_2.classList.add('answerHighlightedRed');
-                        }
-                        else if(q.choosedAnswer === q.answer3){
-                            answer_3.classList.add('answerHighlightedRed');
-                        }
-                        else{
-                            answer_4.classList.add('answerHighlightedRed');
-                        }
-
-
-                        if(q.correctAnswer === q.answer1){
-                            answer_1.classList.add('answerHighlightedGreen');
-                        }
-                        else if(q.correctAnswer === q.answer2){
-                            answer_2.classList.add('answerHighlightedGreen');
-                        }
-                        else if(q.correctAnswer === q.answer3){
-                            answer_3.classList.add('answerHighlightedGreen');
-                        }
-                        else{
-                            answer_4.classList.add('answerHighlightedGreen');
-                        }
-
-                        incorrectAnswersCounter ++;
+                        answer_4.classList.add('answerHighlightedGreen');
                     }
-                    
 
-                    questionContainer.appendChild(questionn);
-                    questionContainer.appendChild(score);
+                    correctAnswersCounter ++;
+                }
+                else{
+                    score.className = 'summaryFailedQuestionScore';
+                    score.textContent = '+ 0';
+                    mainQuestionContainer.classList.add('red');
 
-                    answersContainer.appendChild(answer_1);
-                    answersContainer.appendChild(answer_2);
-                    answersContainer.appendChild(answer_3);
-                    answersContainer.appendChild(answer_4);
+                    if(q.choosedAnswer === q.answer1){
+                        answer_1.classList.add('answerHighlightedRed');
+                    }
+                    else if(q.choosedAnswer === q.answer2){
+                        answer_2.classList.add('answerHighlightedRed');
+                    }
+                    else if(q.choosedAnswer === q.answer3){
+                        answer_3.classList.add('answerHighlightedRed');
+                    }
+                    else{
+                        answer_4.classList.add('answerHighlightedRed');
+                    }
 
-                    mainQuestionContainer.appendChild(questionContainer);
-                    mainQuestionContainer.appendChild(answersContainer);
 
-                    mainConatiner.appendChild(mainQuestionContainer);
+                    if(q.correctAnswer === q.answer1){
+                        answer_1.classList.add('answerHighlightedGreen');
+                    }
+                    else if(q.correctAnswer === q.answer2){
+                        answer_2.classList.add('answerHighlightedGreen');
+                    }
+                    else if(q.correctAnswer === q.answer3){
+                        answer_3.classList.add('answerHighlightedGreen');
+                    }
+                    else{
+                        answer_4.classList.add('answerHighlightedGreen');
+                    }
+
+                    incorrectAnswersCounter ++;
+                }
+                
+
+                questionContainer.appendChild(questionn);
+                questionContainer.appendChild(score);
+
+                answersContainer.appendChild(answer_1);
+                answersContainer.appendChild(answer_2);
+                answersContainer.appendChild(answer_3);
+                answersContainer.appendChild(answer_4);
+
+                mainQuestionContainer.appendChild(questionContainer);
+                mainQuestionContainer.appendChild(answersContainer);
+
+                mainConatiner.appendChild(mainQuestionContainer);
 
             })
 
@@ -358,6 +433,7 @@ function displayResult(){
 
     let displayScore = document.getElementById('score');
     displayScore.innerHTML = score + '/' + (condition == 'unlimited' ? ((checkQuestions - 1)*10) : (checkQuestions*10));
+    // displayScore.innerHTML = score + '/' + (Qcounter - 1)*10;
 }
 
 
@@ -387,12 +463,15 @@ function restartQuiz(){
     questions = [];
     printed = false;
     Qcounter = 0;
+    checkTimeStart = false;
+    clearInterval(countdown);
     
     document.getElementById('final-score-container').style.display = 'none';
     document.getElementById('score-info-container').style.display = 'none';
     document.getElementById('loading-page').style.display = 'flex';
     document.getElementById('score-info').style.visibility = 'visible';
     document.getElementById('mainBox').style.border = '2px solid #000';
+    document.getElementById('outOfTime').style.display = 'none';
     
     if(condition === 'limited'){
         document.getElementById('settingICON').style.visibility = 'hidden';
@@ -415,6 +494,9 @@ function editSettings(){
     questions = [];
     printed = false;
     Qcounter = 0;
+    checkTimeStart = false;
+    timer.checked = false;
+    clearInterval(countdown);
 
 
     document.getElementById('quizContainer').style.display = 'none';
@@ -425,6 +507,7 @@ function editSettings(){
     document.getElementById('final-score-container').style.display = 'none';
     document.getElementById('exit-unlimitedQuiz-container').style.display = 'none';
     document.getElementById('summaryMainBoxContainer').innerHTML = '';
+    document.getElementById('outOfTime').style.display = 'none';
 
 }
 
